8ad33aedec57f5c4f86e4303075a0991c0233987
AbraDinx <abrahamabilatena@gmail.com>
2025-03-21 18:11:02 -0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('mousemove', function(e) {
+  const link = document.getElementById('floating-link');
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  link.style.left = mouseX + 20 + 'px';  
+  link.style.top = mouseY + 'px';  
+});
